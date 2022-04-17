16861bec2498e7d1811a6319d4115044df00a17d
Yehonatan-Albert <83726826+Yehonatan-Albert@users.noreply.github.com>
2022-04-18 02:22:10 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// https://breslev.co.il/categories-video/קליפים/בדיחות
+
+const folder = 'בדיחות הרב ארוש'
+
+await import('https://cdnjs.cloudflare.com/ajax/libs/jszip/3.9.1/jszip.min.js')
+const zip = new JSZip()
+const videos = document.querySelectorAll('a.img_position[href^="https://breslev.co.il/video/"]')
+console.clear()
+for (const [i, e] of videos.entries()) {
+    try {
+        const link = (await fetch(e.href).then(r => r.text())).match(/https:\/\/.+\.mp4/)[0]
+        const arrayBuffer = await fetch(link).then(r => r.arrayBuffer())
+        const name = `${decodeURIComponent(e.href).match(/[א-ת-]+/)[0]}.mp4`
+        zip.file(`${folder}/${name}`, arrayBuffer)
+        console.clear()
+        console.log(`מכין להורדה... ${i + 1} מתוך ${videos.length} סרטונים`)
+    } catch {
+        continue
+    }
+}
+console.clear()
+console.log('מוריד...')
+const a = document.createElement('a')
+a.href = URL.createObjectURL(await zip.generateAsync({ type: 'blob' }))
+a.download = `${folder}.zip`
+a.click()
